99e3845a7da19745b469bc08a52ee72fa5ee116e
ankita7141 <ankitagosavi471@gmail.com>
2026-01-20 13:06:53 +0530

    pattern printing

---
+++ pattern1.java
@@ -0,0 +1,14 @@
+
+
+class pattern1{
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+
+            for(int k=1;k<=n;k++){
+            System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ pattern2.java
@@ -0,0 +1,18 @@
+class pattern2{
+    public static void main(String[] args) {
+        int n=4;//rows
+        int m=5;//column
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1 || j==1 || i==n || j==m){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
