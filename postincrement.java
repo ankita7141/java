7255bf78e6f41f8313d4235bdb860220fa1fa9d3
ankita7141 <ankitagosavi471@gmail.com>
2026-02-03 23:14:22 +0530

    operators

---
+++ postincrement.java
@@ -0,0 +1,16 @@
+class postincrement{
+public static void main(String args[]){
+
+    int a=10;
+    int b=0;
+    b=a++;
+    System.out.println(a);
+    System.out.println(b);
+    
+
+}
+
+
+
+
+}
